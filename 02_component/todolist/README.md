4db09ec6f61238e3860b768bacbaef83467af22f
ClownYH <qwer1745@gmail.com>
2024-01-18 14:40:22 +0900

    finish todolist

--- 02_component/todolist/README.md
+++ 02_component/todolist/README.md
@@ -1,4 +1,6 @@
 # state 실습
 ## To-Do list 만들기
 
+다크 모드 + 컬러 입력시 글자 색이 바뀌게
+
 ![Alt text](image.png)
--- 02_component/todolist/src/App.js
+++ 02_component/todolist/src/App.js
@@ -1,22 +1,77 @@
 import { useState } from "react";
 
-// 애플리케이션 전체 화면
-function App() {
+const styleFormat = {
+  backgroundColor:"skyblue",
+  height:"50rem",
+  textAlign:"center",
+  position:"relative",
+  fontSize:"20px",
+  btn:{backgroundColor:"blue", color:"white"}
+}
 
-  const styleFormat = {
-    backgroundColor:"skyblue",
-    height:"50rem",
-    textAlign:"center",
-    position:"relative",
-    fontSize:"20px",
-    btn:{backgroundColor:"blue", color:"white"}
-  }
+// FontColorChanger
+const FontColorChanger = ({value, setValue, setFontColor}) => {
+  
+  const onChangeFontColor = e => setValue(e.target.value);
 
-  const [list, setList] = useState([]);
+  const onClickColorChanger = () => {
+    setFontColor(value);
+    setValue("");
+  };
+
+  return (
+    <section>
+    <input type="text" value={value} onChange={onChangeFontColor}/>
+    <button onClick={onClickColorChanger}>색 변경</button>
+  </section>
+  )
+};
+
+// Section1
+const Section1 = ({list, setList, fontColor}) => {
+  
+  const [isChecked, setIsChecked] = useState(false);
+
+  const onChecked = (id) => {
+    setIsChecked((prev) => ({
+      ...prev,
+      [id]:!prev[id]
+    }));
+  };
+
+  const onRemove = id => {
+    
+    const removeList = list.filter(list => list.id !== id);
+    setList(removeList);
+  };
+
+  const toDoList = list.map(
+    list => {
+      return (
+        <li key={list.id}>
+          <input type="checkBox" checked={isChecked[list.id] || false} onChange={() => onChecked(list.id)}/>
+          <label style={{textDecoration: isChecked[list.id]? 'line-through' : 'none'}}>{list.contents}</label>
+          <button style={styleFormat.btn} onClick={() => onRemove(list.id)}>삭제</button>
+        </li>
+      )
+    }
+  );
+
+  return (
+    <section style={fontColor}>
+    <ui>
+      {toDoList}
+    </ui>
+  </section>
+  )
+}
+
+// Section2
+const Section2 = ({list, setList, fontColor}) => {
 
   const [id, setId] = useState(0);
+
   const [contents, setContents] = useState("");
-  const [isChecked, setIsChecked] = useState(false);
 
   const onChangeHandler = e => setContents(e.target.value);
   const onClickHandler = () => {
@@ -30,46 +85,62 @@ function App() {
     setContents("");
   };
 
-  const onChecked = () => {
-    setIsChecked(!isChecked);
+  return (
+    <section style={fontColor}>
+    <input onChange={onChangeHandler} value={contents}/>
+    <button style={styleFormat.btn} onClick={onClickHandler}>추가</button>
+  </section>
+  )
+}
+
+// 애플리케이션 전체 화면
+function App() {
+
+  const [list, setList] = useState([]);
+
+  const [fontColor, setFontColor] = useState("");
+
+  const [value, setValue] = useState("");
+
+  const styleColor = {
+    color : fontColor
   };
 
-  const styleChange = {
-    textDecoration: isChecked? 'line-through' : 'none'
-  };
+  const [background, setBackground] = useState({});
 
-  const onRemove = id => {
-    
-    const removeList = list.filter(list => list.id !== id);
-    setList(removeList);
-  };
+  const onBlackmode = () => {
+    if(Object.keys(background).length === 0){
+      setBackground({
+        color:"white",
+        backgroundColor:"black",
+        height:"50rem",
+        textAlign:"center",
+        position:"relative",
+        fontSize:"20px"
+      })
+    }else{
+      setBackground({
 
-  const toDoList = list.map(
-    list => {
-      return (
-        <li key={list.id} style={styleChange}>
-          <input type="checkbox" checked={isChecked} onChange={onChecked}/>{list.contents}
-          <button style={styleFormat.btn} onClick={() => onRemove(list.id)}>삭제</button>
-        </li>
-      )
+      })
     }
-  );
+    
+  };
 
   return (
     <>
-      <div style={styleFormat}>
-        <h1>To do list</h1>
+      <div style={Object.keys(background).length === 0? styleFormat : background}>
+        <div>
+          <h1>To do list</h1>
 
-        <section>
-          <ui>
-            {toDoList}
-          </ui>
-        </section>
+          <button onClick={onBlackmode}>다크 모드</button>
 
-        <section>
-          <input onChange={onChangeHandler} value={contents}/>
-          <button style={styleFormat.btn} onClick={onClickHandler}>추가</button>
-        </section>
+          <FontColorChanger value={value} setValue={setValue} setFontColor={setFontColor}/>
+
+          <Section1 list={list} setList={setList} fontColor={styleColor}/>
+
+          <Section2 list={list} setList={setList} fontColor={styleColor}/>
+          
+        </div>
       </div>
     </>
   );
