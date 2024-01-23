ed5f0cd29111234eabea3cc405fab80d502fd76a
ClownYH <qwer1745@gmail.com>
2024-01-23 18:28:08 +0900

    modify skills, ~ing projects

--- introduce/src/projects.js
+++ introduce/src/projects.js
@@ -1,3 +1,9 @@
+import './projects.css'
+import project1 from './img/projects/bunnyroom/토끼로고.jpg'
+import architecture from './img/projects/bunnyroom/archi.png'
+import db from './img/projects/bunnyroom/db.png'
+import site from './img/projects/bunnyroom/site.png'
+
 function Projects(){
 
     return (
@@ -7,9 +13,27 @@ function Projects(){
                 <p>Projects.</p>
             </h2>
 
-            <div>
-                <article>
-                    프로젝트들
+            <div className='projectList'>
+                <article className="project">
+                    <img className='mainImg' src={project1}/>
+                    <div className='description'>
+                        <h3 className='prSubTitle'>Semi Project</h3>
+                        <h1 className='prTitle'>Bunny Room</h1>
+                        <hr/>
+                        <br/>
+                        <h4>소개</h4>
+                        <hr/>
+                        <p>
+                            첫 프로젝트로 공간 대여 서비스 '버니룸(Bunny Room)'에 참여하여 
+                            관리자의 회원, 블랙리스트, 업체, 홈페이지 설정 등을 관리하는 기능 제작하는 파트를 주로 담당했으며, 일반 이용자가 사용하는 마이페이지 기능을 보조하였고, 프로젝트의 CSS 전반에 참여, 제작하였습니다.<br/>
+                        </p>
+                        <br/>
+                        <h4>참고 자료</h4>
+                        <hr/>
+                        <img className='refImg' src={architecture}/>
+                        <img className='refImg' src={db}/>
+                        <img className='refImg' src={site}/>
+                    </div>
                 </article>
             </div>
         </section>
--- introduce/src/skills.js
+++ introduce/src/skills.js
@@ -1,64 +1,87 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import './skills.css'
+import { faCss3Alt, faGithub, faHtml5, faJava, faJs, faReact } from '@fortawesome/free-brands-svg-icons'
+import { faCirclePlus, faDatabase, faLeaf, faPowerOff } from '@fortawesome/free-solid-svg-icons'
+import coffee from './img/coffee.png'
 
 function Skills () {
-    const skillName = [""]
+    const skills = {
+        html5 : {
+            name : "HTML5",
+            icon : <FontAwesomeIcon icon={faHtml5} size='8x'/>
+        },
 
+        css3 : {
+            name: "CSS3",
+            icon : <FontAwesomeIcon icon={faCss3Alt} size="8x" />
+        },
+
+        js : {
+            name : "JavaScript",
+            icon : <FontAwesomeIcon icon={faJs} size='8x'/>
+        },
+
+        react : {
+            name : "React",
+            icon : <FontAwesomeIcon icon={faReact} size='8x'/>
+        },
+
+        java : {
+            name : "Java",
+            icon : <FontAwesomeIcon icon={faJava} size='8x'/>
+        },
+
+        spring : {
+            name : "Spring",
+            icon : <FontAwesomeIcon icon={faLeaf} size='8x'/>
+        },
+
+        springBoot : {
+            name : "Spring boot",
+            icon : <FontAwesomeIcon icon={faPowerOff} size='8x'/>
+        },
+
+        mySql : {
+            name : "MySQL",
+            icon : <FontAwesomeIcon icon={faDatabase} size='8x'/>
+        },
+
+        github : {
+            name : "Github",
+            icon : <FontAwesomeIcon icon={faGithub} size='8x'/>
+        },
+
+        toBeAdded : {
+            name : "...To be Added!!",
+            icon : <FontAwesomeIcon icon={faCirclePlus} size='8x'/>
+        }
+    }
+
+    const skillList = () => {
+        // Object 객체로 변환(배열이 아니기 때문에 직접 map을 적용할 수 없다.)
+        return Object.values(skills).map((skill, index) => (
+            <div key={index} className='skill'>
+                {skill.icon}
+                <label>{skill.name}</label>
+            </div>
+        ))
+        
+    }
+    
     return (
         <section id="skills">
             <h2>
                 <p className='subTitle'>스킬.</p>
                 <p>Skills.</p>
             </h2>
-
+            <div id='skillContents'>
             <article id='skillTree'>
+                <img id='coffee' src={coffee} draggable='false'/>
                 <div className='skillTable'>
-                    <div className='skill'>
-                        <label>
-                            HTML
-                        </label>
-                    </div>
-                    <div className='skill'>
-                        <label>
-                            JAVASCRIPT
-                        </label>
-                    </div>
-                    <div className='skill'>
-                        <label>
-                            JAVA
-                        </label>
-                    </div>
-
-                    <div className='skill'>
-                        <label>
-                            JAVA
-                        </label>
-                    </div>
-
-                    <div className='skill'>
-                        <label>
-                            JAVA
-                        </label>
-                    </div>
-
-                    <div className='skill'>
-                        <label>
-                            JAVA
-                        </label>
-                    </div>
-
-                    <div className='skill'>
-                        <label>
-                            JAVA
-                        </label>
-                    </div>
-
-                    <div className='skill'>
-                        <label>
-                            JAVA
-                        </label>
-                    </div>
+                    {skillList()}
                 </div>
             </article>
+            </div>
         </section>
     )
 }
