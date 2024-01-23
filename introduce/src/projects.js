ecf73b7393e87d6a9e68427b83c4e22700883c55
ClownYH <qwer1745@gmail.com>
2024-01-23 22:50:06 +0900

    modify projects

--- introduce/src/projects.js
+++ introduce/src/projects.js
@@ -3,8 +3,56 @@ import project1 from './img/projects/bunnyroom/토끼로고.jpg'
 import architecture from './img/projects/bunnyroom/archi.png'
 import db from './img/projects/bunnyroom/db.png'
 import site from './img/projects/bunnyroom/site.png'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faChessPawn } from '@fortawesome/free-solid-svg-icons'
 
 function Projects(){
+
+    const projects = {
+        bunnyroom : {
+            prSubTitle : "Semi Projects",
+            prTitle : "Bunny Room",
+            description : "첫 프로젝트로 공간 대여 서비스 '버니룸(Bunny Room)'에 참여하여 관리자의 회원, 블랙리스트, 업체, 홈페이지 설정 등을 관리하는 기능 제작하는 파트를 주로 담당했으며, 일반 이용자가 사용하는 마이페이지 기능을 보조하였고, 프로젝트의 CSS 전반에 참여, 제작하였습니다.",
+            mainImg : project1,
+            refImg : [architecture, db, site],
+            github: "https://github.com/yycBunnyRoom/SEMI_PROJECT-BUNNY_ROOM"
+        }
+    }
+
+    const projectList = () => {
+        return Object.values(projects).map((project, index) => (
+                <article key={index} className="project">
+                    <img className='mainImg' src={project.mainImg}/>
+                    <div className='description'>
+                        <h3 className='prSubTitle'>{project.prSubTitle}</h3>
+                        <h1 className='prTitle'>{project.prTitle}</h1>
+                        <hr/>
+                        <br/>
+                        <h4><FontAwesomeIcon icon={faChessPawn} /> 소개</h4>
+                        <hr/>
+                        <p>
+                            {project.description}
+                            <br/>
+                        </p>
+                        <br/>
+                        <h4><FontAwesomeIcon icon={faChessPawn} /> 참고 자료</h4>
+                        <hr/>
+                        {project.refImg.map((img, i) => (
+                            <img key={i} className='refImg' src={img}/>
+                        ))}
+                        <br/>
+                        <br/>
+                        <h4><FontAwesomeIcon icon={faChessPawn} /> 링크</h4>
+                        <hr/>
+                        {/* rel 속성의 noopener는 현재 활성화된 페이지와 새로운 탭에서 활성화되는 페이지를 별개의 프로세스 퓌급,
+                             noreferrer는 현재 활성화된 페이지의 정보를 새로운 탭에서 활성화되는 페이지에 전달하지 않음*/}
+                        <a href={project.github} target='blank' rel="noopener noreferrer"><strong>{project.prTitle}</strong></a>
+
+                    </div>
+                </article>
+        ))
+
+    }
 
     return (
         <section id="projects">
@@ -14,27 +62,7 @@ function Projects(){
             </h2>
 
             <div className='projectList'>
-                <article className="project">
-                    <img className='mainImg' src={project1}/>
-                    <div className='description'>
-                        <h3 className='prSubTitle'>Semi Project</h3>
-                        <h1 className='prTitle'>Bunny Room</h1>
-                        <hr/>
-                        <br/>
-                        <h4>소개</h4>
-                        <hr/>
-                        <p>
-                            첫 프로젝트로 공간 대여 서비스 '버니룸(Bunny Room)'에 참여하여 
-                            관리자의 회원, 블랙리스트, 업체, 홈페이지 설정 등을 관리하는 기능 제작하는 파트를 주로 담당했으며, 일반 이용자가 사용하는 마이페이지 기능을 보조하였고, 프로젝트의 CSS 전반에 참여, 제작하였습니다.<br/>
-                        </p>
-                        <br/>
-                        <h4>참고 자료</h4>
-                        <hr/>
-                        <img className='refImg' src={architecture}/>
-                        <img className='refImg' src={db}/>
-                        <img className='refImg' src={site}/>
-                    </div>
-                </article>
+                {projectList()}
             </div>
         </section>
     )
