a66b78feb7dbdac078d4b4fb08722da8f30b2d14
ClownYH <qwer1745@gmail.com>
2024-01-30 19:59:54 +0900

    modify ver4

--- introduce/src/common/footer.js
+++ introduce/src/common/footer.js
@@ -5,7 +5,8 @@ const Footer = () => {
     return (
         <>
             <footer>
-                copyright 2024. by Yuhwan Jeong
+                <span> &copy;copyright 2024. by Yuhwan Jeong</span><br/>
+                <span>본 포트폴리오는 상업용으로 사용하지 않습니다.</span>
             </footer>
         </>
     )
--- introduce/src/contact.js
+++ introduce/src/contact.js
@@ -8,27 +8,29 @@ function Contact () {
                 <p className='subTitle'>콘택트.</p>
                 <p>Contact.</p>
             </h2>
-
-            <div id='board1'>
-                <div id='board2'>
-                    <article className='contactBoard'>
-                        <p>
-                            감사합니다.<br/>
-                            언제나 성장하는<br/>
-                            개발자가 되겠습니다.
-                        </p>
-                    </article>
-                    <article className='contactBoard'>
-                        <h3>Contact to</h3>
-                        <hr/>
-                        <br/>
-                        <a href='https://github.com/ClownYH' target='blank'><strong>github</strong> 바로가기</a>
-                        <br/>
-                        <br/>
-                        <a href='mailto:qwer1745@gmail.com'>이메일 보내기</a>
-                    </article>
+            <div id='board'>
+                <div id='board1'>
+                    <div id='board2'>
+                        <article className='contactBoard'>
+                            <p>
+                                감사합니다.<br/>
+                                언제나 성장하는<br/>
+                                개발자가 되겠습니다.
+                            </p>
+                        </article>
+                        <article className='contactBoard'>
+                            <h3>Contact to</h3>
+                            <hr/>
+                            <br/>
+                            <a href='https://github.com/ClownYH' target='blank'><strong>github</strong> 바로가기</a>
+                            <br/>
+                            <br/>
+                            <a href='mailto:qwer1745@gmail.com'>이메일 보내기</a>
+                        </article>
+                    </div>
                 </div>
             </div>
+            
         </section>
     )
 }
